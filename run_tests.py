8abe7f056d69edcb1c063559d31c5110ea1683a0
Fabian Junge <fabsjunge@gmail.com>
2018-05-24 22:14:27 -0500

    adding tests for map_line

--- run_tests.py
+++ run_tests.py
@@ -3,11 +3,13 @@
 
 import unittest
 import tests.test_command_string as test_command_string
+import tests.test_map_line as test_map_line
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     suite.addTests(loader.loadTestsFromModule(test_command_string))
+    suite.addTest(loader.loadTestsFromModule(test_map_line))
 
     runner = unittest.TextTestRunner(verbosity=3)
     result = runner.run(suite)
---
+++ tests/test_map_line.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+import generate_cfg
+
+
+class TestConstructMapLine(TestCase):
+    def setUp(self):
+        pass
+
+    def test_fail(self):
+        self.assertFalse(True)
+
+
+if __name__ == "__main__":
+    """This script can not be run directly as it requires something from its
+    parent directory
+    """
+    pass
